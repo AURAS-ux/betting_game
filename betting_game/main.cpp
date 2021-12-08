b83955c14a4f76a50a4aae03fc7e85fe0943d637
Auras Paltanea <92265800+AurasCrossCross@users.noreply.github.com>
2021-12-09 00:13:31 +0200

    fixed errors with money sums

--- betting_game/main.cpp
+++ betting_game/main.cpp
@@ -12,20 +12,20 @@ int getRandom()
 int main()
 {
 	string playerName;
-	int totalBalance=500;
+	int totalBalance=800;
 	int bettedAmount;
 	int randomNumber = getRandom();
 	int playerNumber;
 	bool shouldEnd = false;
 	cout << "Introduce your name:";
 	std::getline(cin, playerName);
-	while (!shouldEnd && totalBalance>0)
+	while (!shouldEnd && totalBalance>100 && totalBalance>0)
 	{
 		string choice;
 		cout << "How much would you like to bet(a number greater then 100):"; cin >> bettedAmount;
-		if (bettedAmount < 100)
+		if (bettedAmount < 100 || bettedAmount>totalBalance)
 		{
-			cout << "Insuficient money!";
+			cout << "Error proccesing money!";
 			exit(0);
 		}
 		totalBalance -= bettedAmount;
